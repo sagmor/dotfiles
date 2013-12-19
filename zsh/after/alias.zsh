0297069ff62ed7b0a12002857571296e0dc5215c
Seba Gamboa <me@sagmor.com>
2013-12-19 16:53:41 -0300

    Add some aliases

---
+++ zsh/after/alias.zsh
@@ -0,0 +1,12 @@
+# editor
+alias edit="mvim"
+alias ae="edit ~/.zsh.after/aliases.zsh"
+ 
+# Autojump
+alias j="fasd_cd -d"
+ 
+# git
+unalias gst
+alias git="hub"
+alias gl="git pull"
+alias grc='git rebase --continue'
