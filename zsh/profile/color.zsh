672d322273f58998f07346c884e32b52ceef30a4
Seba Gamboa <me@sagmor.com>
2015-10-05 12:49:53 -0300

    Change default theme

--- zsh/profile/color.zsh
+++ zsh/profile/color.zsh
@@ -1,3 +1,3 @@
 # Base16 Shell
-BASE16_SHELL="$HOME/.zsh/plugins/base16-shell/base16-railscasts.dark.sh"
+BASE16_SHELL="$HOME/.zsh/plugins/base16-shell/base16-ocean.dark.sh"
 [[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
--- zsh/settings/theme.zsh
+++
@@ -1,3 +0,0 @@
-# Base16 Shell
-# BASE16_SHELL="$HOME/.zsh/plugins/base16-shell/base16-railscasts.dark.sh"
-# [[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
