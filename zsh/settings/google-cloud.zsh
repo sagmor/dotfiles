812cbc0d51e3006a3a8a6aaaad5cd06d73127120
Seba Gamboa <me@sagmor.com>
2015-12-21 18:18:21 -0300

    Load google cloud cli when available

---
+++ zsh/settings/google-cloud.zsh
@@ -0,0 +1,6 @@
+if [[ -s "/opt/google/google-cloud-sdk" ]]; then
+  # The next line updates PATH for the Google Cloud SDK.
+  source '/opt/google/google-cloud-sdk/path.zsh.inc'
+  # The next line enables shell command completion for gcloud.
+  source '/opt/google/google-cloud-sdk/completion.zsh.inc'
+fi
